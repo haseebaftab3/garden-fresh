c0003adc9412701e73a286a1d26a5d78c77a92bd
haseebaftab3 <haseebaftab3@gmail.com>
2024-12-23 02:22:15 +0500

    Remove unused portfolio components and SVG assets to clean up the codebase

--- resources/views/components/product-details.blade.php
+++ resources/views/components/product-details.blade.php
@@ -643,6 +643,10 @@
 
 @push('css')
     <style>
+        strong {
+            font-weight: inherit !important;
+        }
+
         .loader-overlay {
             position: absolute;
             top: 0;
