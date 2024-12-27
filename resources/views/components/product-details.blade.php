c00e5143af39d14cc02870cd96c27530624d8fb9
haseebaftab3 <haseebaftab3@gmail.com>
2024-12-28 01:38:16 +0500

    Heading Fix

--- resources/views/components/product-details.blade.php
+++ resources/views/components/product-details.blade.php
@@ -383,7 +383,7 @@
                                         });
 
                                         // Add 'text-btn-uppercase mb_12' classes to all heading tags (h1, h2, h3, h4)
-                                        const headingTags = contentDiv.querySelectorAll("h1, h2, h3, h4");
+                                        const headingTags = contentDiv.querySelectorAll("h1, h2, h3, h4,strong");
                                         headingTags.forEach(heading => {
                                             heading.classList.add("text-btn-uppercase", "mb_12");
                                         });
@@ -578,8 +578,8 @@
 @push('css')
     <style>
         /* strong {
-                                                                                                                                                                                                                                    font-weight: inherit !important;
-                                                                                                                                                                                                                                } */
+                                                                                                                                                                                                                                            font-weight: inherit !important;
+                                                                                                                                                                                                                                        } */
 
         .loader-overlay {
             position: absolute;
