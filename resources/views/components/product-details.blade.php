b71fac27dabe05cb1a674a3ee71c6e08c589f424
haseebaftab3 <haseebaftab3@gmail.com>
2024-12-28 00:14:24 +0500

    Ceil on discount

--- resources/views/components/product-details.blade.php
+++ resources/views/components/product-details.blade.php
@@ -150,7 +150,7 @@
 
                                         @if (!empty($product->discount) && $product->discount > 0)
                                             <div class="badges-on-sale text-btn-uppercase">
-                                                {{ round($product->discount, 1) }}%
+                                                {{ ceil($product->discount) }}%
                                             </div>
                                         @endif
                                     </div>
@@ -555,8 +555,8 @@
 @push('css')
     <style>
         /* strong {
-                                                                                                                                                                                                                        font-weight: inherit !important;
-                                                                                                                                                                                                                    } */
+                                                                                                                                                                                                                                font-weight: inherit !important;
+                                                                                                                                                                                                                            } */
 
         .loader-overlay {
             position: absolute;
